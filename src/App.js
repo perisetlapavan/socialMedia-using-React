175c9b1727f761b93898018480fdd4edcb8f3f19
perisetlapavan <perisetlapavan@gmail.com>
2022-06-08 14:16:44 +0530

    designed add icon with its modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Stack } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import Add from './components/Add'
 import Feed from './components/Feed'
 import Navbar from './components/Navbar'
 import Rightbar from './components/Rightbar'
@@ -17,6 +18,7 @@ const App = () => {
                 <Feed />
                 <Rightbar />
             </Stack>
+            <Add/>
         </Box>
     )
 }
---
+++ src/components/Add.jsx
@@ -0,0 +1,67 @@
+import styled from '@emotion/styled';
+import { EmojiEmotions, PersonAdd, VideoCameraBack, Image, DateRange } from '@mui/icons-material';
+import AddIcon from '@mui/icons-material/Add';
+import { Avatar, Box, Button, ButtonGroup, Fab, Modal, Stack, TextField, Tooltip, Typography } from '@mui/material'
+import React, { useState } from 'react'
+
+const StyledModal = styled(Modal)({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+})
+
+const UserBox = styled(Box)({
+    display: 'flex',
+    alignItems: 'center',
+    gap: "10px",
+    marginBottom: "20px"
+})
+const Add = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <>
+            <Tooltip onClick={e => setOpen(true)} title="Add" sx={{ position: "fixed", bottom: 20, left: { xs: "calc(50% - 25px)", md: 30 }, }}>
+                <Fab color="primary" aria-label="add">
+                    <AddIcon />
+                </Fab>
+            </Tooltip>
+            <StyledModal
+                open={open}
+                onClose={e => setOpen(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+                    <Typography variant="h6" color="gray" textAlign="center">
+                        Create Post
+                    </Typography>
+                    <UserBox>
+                        <Avatar src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Nandamuri_Balakrishna.jpg/220px-Nandamuri_Balakrishna.jpg"
+                            sx={{ height: 30, width: 30 }} />
+                        <Typography fontWeight={500} variant="span">Bala krishna</Typography>
+                    </UserBox>
+                    <TextField
+                        sx={{ width: "100%" }}
+                        id="standard-multiline-static"
+                        multiline
+                        rows={3}
+                        placeholder="What's on your mind?"
+                        variant="standard"
+                    />
+                    <Stack direction="row" gap={1} mt={2} mb={3}>
+                        <EmojiEmotions color="primary" />
+                        <Image color="secondary" />
+                        <VideoCameraBack color="success" />
+                        <PersonAdd color="error" />
+                    </Stack>
+                    <ButtonGroup fullWidth variant="contained" aria-label="outlined primary button group">
+                        <Button>Post</Button>
+                        <Button sx={{width:"100px"}}><DateRange/></Button>
+                    </ButtonGroup>
+                </Box>
+            </StyledModal>
+        </>
+    )
+}
+
+export default Add
