8314dab7bfd22d38eb8ae1fa8865d1c9394ade80
perisetlapavan <perisetlapavan@gmail.com>
2022-06-08 09:46:36 +0530

    designed profile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import { AppBar, Avatar, Badge, InputBase, styled, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Avatar, Badge, InputBase, Menu, MenuItem, styled, Toolbar, Typography } from '@mui/material'
+import React ,{useState} from 'react'
 import PetsIcon from '@mui/icons-material/Pets';
 import { Box } from '@mui/system';
 import MailIcon from '@mui/icons-material/Mail';
 import { Notifications } from '@mui/icons-material';
+
 
 // this helps in preventing inline styling to Toolbar that has properties fixed, same for below styled
 const StyledToolBar = styled(Toolbar)({
@@ -21,11 +22,11 @@ const Search = styled("div")(({ theme }) => ({
 const Icons = styled(Box)(({ theme }) => ({
     display: "none",
     gap: "20px",
-    alignItems: "center" ,
+    alignItems: "center",
     // breakpoints to make responsiveness possible
     [theme.breakpoints.up('sm')]: {
         display: "flex",
-      },
+    },
 
 }));
 
@@ -36,10 +37,12 @@ const UserBox = styled(Box)(({ theme }) => ({
     // breakpoints to make responsiveness possible
     [theme.breakpoints.up('sm')]: {
         display: "none",
-      },
+    },
 }));
 
 const Navbar = () => {
+    // open state is used to open and close the menu of balayya profile. see open attribute in menu tag of profile
+    const [open,setOpen]=useState(false)
     return (
         // Appbar gives bar while Toolbar gives padding effect to Appbar. static in position attribute makes components 
         // that are underneath it go below so it becomes visible.
@@ -57,15 +60,35 @@ const Navbar = () => {
                     <Badge badgeContent={4} color="error">
                         <Notifications />
                     </Badge>
-                    <Avatar sx={{ width: 30, height: 30 }} src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Nandamuri_Balakrishna.jpg/220px-Nandamuri_Balakrishna.jpg' />
+                    <Avatar 
+                          onClick={e=>setOpen(true)}
+                          sx={{ width: 30, height: 30 }} src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Nandamuri_Balakrishna.jpg/220px-Nandamuri_Balakrishna.jpg' />
                 </Icons>
-                <UserBox>
+                <UserBox onClick={e=>setOpen(true)}>
                     <Avatar sx={{ width: 30, height: 30 }} src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Nandamuri_Balakrishna.jpg/220px-Nandamuri_Balakrishna.jpg' />
                     <Typography variant="span">
                         Balayya
                     </Typography>
                 </UserBox>
             </StyledToolBar>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={open}
+                onClose={e=>setOpen(false)}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+            >
+                <MenuItem >Profile</MenuItem>
+                <MenuItem >My account</MenuItem>
+                <MenuItem >Logout</MenuItem>
+            </Menu>
         </AppBar>
     )
 }
